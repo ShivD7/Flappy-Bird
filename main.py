27049d69c51f31ae4935bb5ca8f0a19f5b9ef3a5
ShivD7 <97405847+ShivD7@users.noreply.github.com>
2023-06-04 15:19:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
-import time
 pygame.init()
 size = (400, 600)
 screen = pygame.display.set_mode(size)
 
+main_characterx2 = 60
+main_charactery2 = 250
 
-main_characterx = 40
-main_charactery = 350
+score = 0
+times_clicked = 0
+
+moveup = None
+
+
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 SKY_BLUE = (3, 252, 252)
 BLACK = (0, 0, 0)
 
+playing = False
 done = False
+if_starting = True
+if_playing = False
+
+font = pygame.font.Font("FlappybirdyRegular-KaBW.ttf", 100)
 
 main_character = pygame.image.load("jetpackman.png")
 main_character = pygame.transform.scale(main_character, [150, 175])
@@ -21,30 +31,77 @@ background = pygame.image.load("background.jpg")
 background = pygame.transform.scale(background, [400, 600])
 
 intro_text = pygame.image.load("Screen Shot 2023-06-04 at 11.02.54 AM.png")
+intro_text = pygame.transform.scale(intro_text, [400, 400])
+
+clock = pygame.time.Clock()
+
 
 
 def starting_screen():
-    main_characterx = 120
-    main_charactery = 280
-    screen.fill(WHITE)
-    font = pygame.font.Font("FlappybirdyRegular-KaBW.ttf", 100)
-    screen.blit(intro_text, [25, 200])
-    screen.blit(main_character, [main_characterx, main_charactery])
-    main_charactery -= 2
-    time.sleep(60)
-    main_charactery += 2
-    pygame.display.flip()
-
-def playing_screen():
-    screen.fill(BLACK)
-    pygame.display.flip()
-
-while not done:
-    starting_screen()
+    global if_starting
+    global if_playing
+    global done
+    global times_clicked
+    main_characterx1 = 120
+    main_charactery1 = 280
+    screen.blit(background, [0, 0])
+    screen.blit(intro_text, [1, 200])
+    screen.blit(main_character, [main_characterx1, main_charactery1])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
-            playing_screen()
+            times_clicked += 1
+            if_starting = False
+            if_playing = True
+
+    pygame.display.flip()
+    clock.tick(60)
+
+
+def playing_screen():
+    global times_clicked
+    global score
+    global done
+    global moveup
+    global main_characterx2
+    global main_charactery2
+    screen.blit(background, [0, 0]) 
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                times_clicked += 1
+                if times_clicked != 1:
+                    moveup = True
+                    score += 1
+                else:
+                    moveup = None
+            if event.type == pygame.MOUSEBUTTONUP:
+                if times_clicked != 1:
+                    moveup = False
+                else:
+                    moveup = None
+      
+    if moveup == True:
+        main_charactery2 -= 20
+    if moveup == False: 
+        main_charactery2 += 10
+    if moveup == None:
+        main_charactery2 == main_charactery2
+    screen.blit(main_character, [main_characterx2, main_charactery2])
+    font2 = pygame.font.SysFont("Calibri", 50, False, False)
+    score_text = font2.render(str(score), True, WHITE)
+    screen.blit(score_text, [185, 100]) 
+    pygame.display.flip()
+    clock.tick(60)
+
+while not done:
+    if if_starting:
+        starting_screen()
+    if if_playing:
+        playing_screen()
+
+    clock.tick(60)
 
 pygame.quit()
