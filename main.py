32d43302af53a6a6ec6fdf5840501b49742fe5dc
Shiv Desai <97405847+ShivD7@users.noreply.github.com>
2023-06-07 19:10:39 -0400

    Changed entire format of code to reduce lines; it still works the same. I also added an "infinite" sidewalk, meaning the ground would constantly move and would never end until the player loses.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+import random
+import time
 pygame.init()
 size = (400, 600)
 screen = pygame.display.set_mode(size)
 
+game_state = "intro"
+
 collided = False
+drawing_poles = False
 
 main_characterx2 = 60
 main_charactery2 = 250
@@ -11,18 +16,30 @@ main_charactery2 = 250
 score = 0
 times_clicked = 0
 
-sidewalk_x = 0
 
-up_speed = 10
+
+up_speed = 5
 down_speed = 3
 
 moveup = None
 
-skyscraper_xpos = 450
+skyscraper_xpos = 650
 skyscraper_ypos = 0
 
-skyscraper2_xpos = 450
+skyscraper2_xpos = 650
 skyscraper2_ypos = 250
+
+skyscraper3_xpos = 650
+skyscraper3_ypos = 0
+
+skyscraper4_xpos = 650
+skyscraper4_ypos = 480
+
+skyscraper5_xpos = 650
+skyscraper5_ypos = 0
+
+skyscraper6_xpos = 650
+skyscraper6_ypos = 400
 
 
 BLUE = (0, 0, 255)
@@ -32,9 +49,6 @@ BLACK = (0, 0, 0)
 
 playing = False
 done = False
-if_starting = True
-if_playing = False
-if_ending = False
 
 font = pygame.font.Font("FlappybirdyRegular-KaBW.ttf", 100)
 
@@ -45,124 +59,136 @@ background = pygame.transform.scale(background, [400, 600])
 intro_text = pygame.image.load("Screen Shot 2023-06-04 at 11.02.54 AM.png")
 intro_text = pygame.transform.scale(intro_text, [400, 400])
 
-skyscraper = pygame.image.load("building.png")
-skyscraper = pygame.transform.scale(skyscraper, [150, 100])
-
+skyscraper = pygame.image.load("building2.png")
+skyscraper = pygame.transform.scale(skyscraper, [100, 100])
 skyscraper2 = pygame.image.load("building2.png")
-skyscraper2 = pygame.transform.scale(skyscraper2, [150, 350])
+skyscraper2 = pygame.transform.scale(skyscraper2, [100, 350])
+
+skyscraper3 = pygame.image.load("building2.png")
+skyscraper3 = pygame.transform.scale(skyscraper3, [100, 370])
+skyscraper4 = pygame.image.load("building.png")
+skyscraper4 = pygame.transform.scale(skyscraper4, [100, 120])
+
+skyscraper5 = pygame.image.load("building2.png")
+skyscraper5 = pygame.transform.scale(skyscraper5, [100, 230])
+skyscraper6 = pygame.image.load("building2.png")
+skyscraper6 = pygame.transform.scale(skyscraper6, [100, 230])
+
+skyscraper_list = [[skyscraper], [skyscraper2],
+                   [skyscraper3], [skyscraper4],
+                   [skyscraper5], [skyscraper6]]
 
 sidewalk = pygame.image.load("Sidewalk.jpg")
 sidewalk = pygame.transform.scale(sidewalk, [600, 100])
 
+sidewalk_x = 0
+sidewalk_x2 = sidewalk.get_width()
+
 clock = pygame.time.Clock()
 
 
-def ending_screen():
-    global done
-    global font
-    font = pygame.font.Font("FlappybirdyRegular-KaBW.ttf", 100)
-    font2 = pygame.font.SysFont("Calibri", 50, False, False)
-    losing_text = font.render("YOU DIED", True, WHITE)
-    score_text = font2.render(str(score), True, WHITE)
-    score_text2 = font.render("SCORE", True, WHITE)
-    screen.fill(BLACK)
-    screen.blit(losing_text, [80, 100])
-    screen.blit(score_text2, [120, 250])
-    screen.blit(score_text, [178, 325])
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True    
-    pygame.display.flip()
-
-
-def starting_screen():
-    global if_starting
-    global if_playing
-    global done
-    global times_clicked
-    main_character = pygame.image.load("jetpackman.png")
-    main_character = pygame.transform.scale(main_character, [150, 175])
-    main_characterx1 = 120
-    main_charactery1 = 280
-    screen.blit(background, [0, 0])
-    screen.blit(intro_text, [1, 200])
-    screen.blit(main_character, [main_characterx1, main_charactery1])
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            times_clicked += 1
-            if_starting = False
-            if_playing = True
-
-    pygame.display.flip()
-
-
-def playing_screen():
-    global collided
-    global skyscraper_xpos
-    global skyscraper2_xpos
-    global skyscraper2_ypos
-    global skyscraper2_ypos
-    global times_clicked
-    global score
-    global done
-    global moveup
-    global main_characterx2
-    global main_charactery2
-    global if_playing
-    global if_ending
-    global sidewalk_x
-    screen.blit(background, [0, 0])
+def infinite_sidewalk():
     screen.blit(sidewalk, [sidewalk_x, 500])
-    sidewalk_x -= 1
-    if sidewalk_x + 600 < 600:
-        sidewalk_x = 0
-    for event in pygame.event.get():
+    screen.blit(sidewalk, [sidewalk_x2, 500])
+
+
+
+while not done:
+    if game_state == "intro":
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 times_clicked += 1
-                if times_clicked != 1:
-                    moveup = True
-                else:
-                    moveup = None
-            if event.type == pygame.MOUSEBUTTONUP:
-                if times_clicked != 1:
-                    moveup = False
-                else:
-                    moveup = None
-      
-    if moveup == True:
-        main_charactery2 -= up_speed
-    if moveup == False: 
-        main_charactery2 += down_speed
-        if main_charactery2 + 290 >= 800:
-            if_playing = False 
-            if_ending = True       
-    if moveup == None:
-        main_charactery2 == main_charactery2
-    screen.blit(skyscraper, [skyscraper_xpos, skyscraper_ypos])
-    screen.blit(skyscraper2, [skyscraper2_xpos, skyscraper2_ypos])
-    screen.blit(main_character, [main_characterx2, main_charactery2])
-    font2 = pygame.font.SysFont("Calibri", 50, False, False)
-    score_text = font2.render(str(score), True, WHITE)
-    screen.blit(score_text, [185, 100]) 
-    if collided != True:
-        skyscraper2_xpos -= 2
-        skyscraper_xpos -= 2
-    pygame.display.flip()
-
-
-while not done:
-    if if_starting:
-        starting_screen()
-    if if_playing:
+                game_state = "playing"
         main_character = pygame.image.load("jetpackman.png")
-        main_character = pygame.transform.scale(main_character, [100, 100]) 
-        playing_screen()
-    if if_ending:
-        ending_screen()
-    clock.tick(60)
+        main_character = pygame.transform.scale(main_character, [150, 175])
+        main_characterx1 = 120
+        main_charactery1 = 280
+        screen.blit(background, [0, 0])
+        screen.blit(intro_text, [1, 200])
+        screen.blit(main_character, [main_characterx1, main_charactery1])
+        pygame.display.flip()
+    if game_state == "playing":
+        drawing_poles = True
+        main_character = pygame.image.load("jetpackman.png")
+        main_character = pygame.transform.scale(main_character, [75, 75]) 
+        screen.blit(background, [0, 0])
+        sidewalk_x -= 2
+        sidewalk_x2 -= 2 
+        if sidewalk_x < sidewalk.get_width() * -1:
+            sidewalk_x = sidewalk.get_width()
+        if sidewalk_x2 < sidewalk.get_width() * -1:
+            sidewalk_x2 = sidewalk.get_width()
+        infinite_sidewalk()
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    times_clicked += 1
+                    if times_clicked != 1:
+                        moveup = True
+                    else:
+                        moveup = None
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if times_clicked != 1:
+                        moveup = False
+                    else:
+                        moveup = None
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        times_clicked += 1
+                        if times_clicked != 1:
+                            moveup = True
+                        else:
+                            moveup = None
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        times_clicked += 1
+                        if times_clicked != 1:
+                            moveup = True
+                        else:
+                            moveup = None
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                        times_clicked += 1
+                        if times_clicked != 1:
+                            moveup = False
+                        else:
+                            moveup = None
+        
+
+        if moveup == True:
+            main_charactery2 -= up_speed
+        if moveup == False: 
+            main_charactery2 += down_speed
+            if main_charactery2 + 273 >= 800:
+                drawing_poles = False
+                game_state = "ending"      
+        if moveup == None:
+            main_charactery2 == main_charactery2
+
+        screen.blit(main_character, [main_characterx2, main_charactery2])
+        font2 = pygame.font.SysFont("Calibri", 50, False, False)
+        score_text = font2.render(str(score), True, WHITE)
+        screen.blit(score_text, [185, 100])
+
+        pygame.display.flip()
+
+    if game_state == "ending" :
+        font = pygame.font.Font("FlappybirdyRegular-KaBW.ttf", 100)
+        font2 = pygame.font.SysFont("Calibri", 50, False, False)
+        losing_text = font.render("YOU DIED", True, WHITE)
+        score_text = font2.render(str(score), True, WHITE)
+        score_text2 = font.render("SCORE", True, WHITE)
+        screen.fill(BLACK)
+        screen.blit(losing_text, [80, 100])
+        screen.blit(score_text2, [120, 250])
+        screen.blit(score_text, [178, 325])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True    
+        pygame.display.flip()
+        clock.tick(60)
 
 pygame.quit()
